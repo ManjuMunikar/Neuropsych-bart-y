34f98a5990857a38de6eb0a4230ba3b25eae9d11
Prabin Byanjankar <prabinbyanjankar@Prabins-MacBook-Air.local>
2023-09-08 14:40:28 +0545

    Lower the font size

--- app/src/main/java/com/datagrandeur/neuropsych/data/Database.java
+++ app/src/main/java/com/datagrandeur/neuropsych/data/Database.java
@@ -2,7 +2,7 @@ package com.datagrandeur.neuropsych.data;
 
 public class Database {
 
-    public static final String DATABASE_NAME = "bart-y-3.db";
+    public static final String DATABASE_NAME = "barty_0001.db";
     public static final int DATABASE_VERSION = 1;
 
 }
